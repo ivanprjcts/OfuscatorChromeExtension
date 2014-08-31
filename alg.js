aec04f0a5745eb4dc1a034ecfc7107b340d2ebbc
Ivan Martin <ivanmar_91@hotmail.com>
2014-08-31 21:23:34 +0200

    change files location

--- alg.js
+++
@@ -1,7 +0,0 @@
-var obfuscator_alg1 = function(text){
-    return CryptoJS.SHA1(text).toString();
-};
-
-var deobfuscator_alg1 = function(text){
-    return "DEOBFUSCATED MESSAGE (" + text + ")";
-};
